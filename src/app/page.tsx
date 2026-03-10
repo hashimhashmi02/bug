b194a9d9ed9dee7488511eab05e2a5df7e979b31
hashimhashmi02 <hashimhashmi02@gmail.com>
2026-03-11 04:52:52 +0530

    projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
-const page = () => {
-  return (
-    <div className="text-red-300 display-flex justify-center items-center">
-      <Button variant="outline" size="lg" className="rounded-full">NOOR</Button>
-    </div>
-  )
-}
+const Home = () => {
+  return <ProjectsView />
+};
 
-export default page;
+export default Home;
